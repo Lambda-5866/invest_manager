d23469938cc094bc3c5130582c896229e2991da3
Lambda-5866 <sksskek1gh@gmail.com>
2025-10-12 01:51:52 +0900

    Fix: Dockerfile

--- invest_manager/settings.py
+++ invest_manager/settings.py
@@ -40,7 +40,7 @@ CACHES = {
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
