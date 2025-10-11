ea041b2d56e635ce75795e057270f9db96bed074
Lambda-5866 <sksskek1gh@gmail.com>
2025-10-12 00:44:54 +0900

    Fix: Dockerfile

--- invest_manager/settings.py
+++ invest_manager/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = '/app/staticfiles'
+STATIC_ROOT = '/app/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
